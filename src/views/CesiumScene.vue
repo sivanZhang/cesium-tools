b54c557ea3a53c4c10af664ce3e6f351137aa0a6
sivanZhang <445080336@qq.com>
2020-01-02 17:04:33 +0800

    111

--- src/views/CesiumScene.vue
+++ src/views/CesiumScene.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-let Cesium = require('cesium/Cesium')
+let Cesium = require("cesium/Cesium");
 export default {
   name: "CesiumScene",
   data() {
@@ -15,25 +15,23 @@ export default {
   beforeDestroy() {},
   methods: {
     init() {
-      let viewerOption = {
-        geocoder: false, // 地理位置查询定位控件
-        homeButton: false, // 默认相机位置控件
-        timeline: false, // 时间滚动条控件
-        navigationHelpButton: false, // 默认的相机控制提示控件
-        fullscreenButton: false, // 全屏控件
-        scene3DOnly: true, // 每个几何实例仅以3D渲染以节省GPU内存
-        baseLayerPicker: false, // 底图切换控件
-        animation: false // 控制场景动画的播放速度控件
-      };
-      let viewer = new Cesium.Viewer(this.$el, viewerOption);
-      viewer._cesiumWidget._creditContainer.style.display = "none";// 隐藏版权
+      Cesium.Ion.defaultAccessToken =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkODZjOGY4Yy1jOGEzLTRlNzMtYTdlMS03ZWQ5MmE4M2RkMzEiLCJpZCI6MjA0NTYsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1Nzc0NzI0Nzd9.3OfoKjLexR3j8kcepsM_h0hj1vEpS0jd_aw9n-izLKk";
+      const viewer = new Cesium.Viewer("cesiumContainer");
+      const canvas = viewer.scene.canvas;
+      const mouseHander = new Cesium.ScreenSpaceEventHandler(canvas);
+      let windowPosition;
+      mouseHander.setInputAction(function(event) {
+        // 获取鼠标点的windowPosition
+        windowPosition = event.position;
+      }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
     }
   }
 };
 </script>
 
 <style lang='scss' scoped>
-@import url('~cesium/Widgets/widgets.css');
+@import url("~cesium/Widgets/widgets.css");
 #cesiumContainer {
   width: 100%;
   height: 100%;
