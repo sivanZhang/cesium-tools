d612106bc9b9178fe44d60a86b03e786e250dbd8
sivanZhang <445080336@qq.com>
2020-07-27 10:31:19 +0800

    1111

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,25 @@ module.exports = {
     port: 1024
   },
   productionSourceMap: true,
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve('src'),
+      'cesium': path.resolve(__dirname, cesiumSource)
+    }
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' }]),
+    new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Assets'), to: 'Assets' }]),
+    new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }]),
+    new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'ThirdParty/Workers'), to: 'ThirdParty/Workers' }]),
+    new webpack.DefinePlugin({
+      CESIUM_BASE_URL: JSON.stringify('./')
+    })
+  ],
+  module: {
+    unknownContextCritical: false
+  },
   configureWebpack: {
     amd: {
       toUrlUndefined: true
